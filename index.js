9f5c946f53d6b5bd3665a5b06dbe606990a1b8ab
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2015-09-14 05:57:45 -0500

    Simplifico equanimous-broccoli para que tenga solamente lo que necesitamos para este sorteo

--- index.js
+++ index.js
@@ -1,60 +1,18 @@
 var io = require('socket.io').listen(5647);
 io.on('connection', function(socket){
-  console.log('connected');
-  socket.on('say hi', function(msg){
-    console.log(msg);
-    io.emit('say hi', msg);
-  });
+  console.log('user connected');
 
-  socket.on('request::missions', function(msg){
+  socket.on('user::arrives', function(msg){
     console.log(msg);
-    io.emit('request::missions', {
-      message: 'I have no missions for you, yet.',
-      missionsCount: -1
+    io.emit('user::arrives', {
+      message: 'I have no idea how to welcome user, yet.'
     });
   });
 
-  socket.on('request::challenges', function(msg){
+  socket.on('user::responses', function(msg){
     console.log(msg);
-    io.emit('request::challenges', {
-      message: 'I have no challenges for you, yet.',
-      challengesCount: -1
-    });
-  });
-
-  socket.on('request::accomplishmentsByUser', function(msg){
-    console.log(msg);
-    io.emit('request::accomplishmentsByUser', {
-      message: 'I have no accomplishments registred for you, yet.',
-      accomplishmentsCount: -1
-    });
-  });
-
-  socket.on('user::add', function(msg){
-    console.log(msg);
-    io.emit('user::add', {
-      message: 'I have no idea how to add user, yet.'
-    });
-  });
-
-  socket.on('user::login', function(msg){
-    console.log(msg);
-    io.emit('user::login', {
-      message: 'I have no idea how to welcome a user, yet.'
-    });
-  });
-
-  socket.on('user::bye', function(msg){
-    console.log(msg);
-    io.emit('user::bye', {
-      message: 'I have no idea how to say bye to a user, yet.'
-    });
-  });
-
-  socket.on('user::enterCode', function(msg){
-    console.log(msg);
-    io.emit('user::enterCode', {
-      message: 'I have no idea how to enter a code given by a user, yet.'
+    io.emit('user::responses', {
+      message: 'I have no idea how to save user responses, yet.'
     });
   });
 
