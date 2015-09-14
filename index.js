5ef127beef71dc51b037dcdd5094a0b580164c2c
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2015-09-14 17:21:26 -0500

    maybe?

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function readAllRows() {
 
 function countAllRows(){
   console.log("count users");
-  var registredusers = db.query("SELECT COALESCE(MAX(id)+1, 0) FROM users");
+  var registredusers = db.run("SELECT COALESCE(MAX(id)+1, 0) FROM users");
 
   console.log(registredusers + "registred users");
   return registredusers;
